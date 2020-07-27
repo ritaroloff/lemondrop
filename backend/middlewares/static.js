598c6214cbc0c62480fc52da473a8fb9856371b0
Rita Roloff <ritaroloff@Ritas-MacBook-Air.local>
2020-07-27 10:14:14 -0700

    changed files

--- backend/middlewares/static.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-const rootDir = require('../utils/root-dir');
-
-const static = express.static(path.join(rootDir, 'public'));
-
-module.exports = static;
--- backend/middlewares/token.js
+++
@@ -1,7 +0,0 @@
-exports.hasToken = (req, res, next) => {
-  const { accesskey } = req.headers;
-
-  if (accesskey !== '2jXxOvxTsn') return res.sendStatus(403);
-
-  next();
-};
